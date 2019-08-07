0efae8bfca8e9ec1ce4df9730bbade580e02a9a8
Scott Lindsey <scott@scott.to>
2019-08-07 09:16:12 -0700

    fixed test

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -24,6 +24,7 @@ describe('Sample Code Test', () => {
       message: 'Hello world!',
       details: 'The sample code executed correctly',
       environment: 'local',
+      foo: 'some new value',
     }, null, 2);
 
 
